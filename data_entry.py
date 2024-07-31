1b68bf8b40b0d604c5e7c6bccf98ccfe4d4dcbab
nikolaystankov1 <nikolay.stankov1@gmail.com>
2024-07-31 15:47:40 +0300

    add entry function

--- data_entry.py
+++ data_entry.py
@@ -5,13 +5,27 @@ from datetime import datetime
 
 class CSV:
     CSV_FILE = 'finance_data.csv'
+    COLUMNS = ['date', 'amount', 'category', 'description']
 
     @classmethod
     def initialize_csv(cls):
         try:
             pd.read_csv(cls.CSV_FILE)
         except:
-            df = pd.DataFrame(columns=['date', 'amount', 'category', 'description'])
+            df = pd.DataFrame(columns=cls.COLUMNS)
             df.to_csv(cls.CSV_FILE, index=False)
 
+    @classmethod
+    def add_antry(cls, date, amount, category, description):
+        new_entry = {
+            'date': date,
+            'amount': amount,
+            'category': category,
+            'description': description
+        }
+        with open(cls.CSV_FILE, 'a', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=cls.COLUMNS)
+            writer.writer(new_entry)
+        print('Entry added succesfully')
+        
 CSV.initialize_csv()
