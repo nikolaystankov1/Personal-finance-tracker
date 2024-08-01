dccf3ae57a6c98fc62c597c4c261a46cc49c9dc4
nikolaystankov1 <nikolay.stankov1@gmail.com>
2024-08-01 16:11:06 +0300

    get_transactions function

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from data_entry import get_amount, get_date, get_description, get_category
 class CSV:
     CSV_FILE = 'finance_data.csv'
     COLUMNS = ['date', 'amount', 'category', 'description']
+    FORMAT = "%d-%m-%Y"
 
     @classmethod
     def initialize_csv(cls):
@@ -28,6 +29,41 @@ class CSV:
             writer.writerow(new_entry)
         print('Entry added succesfully')
 
+    @classmethod
+    def get_transactions(cls, start_date, end_date):
+        df = pd.read_csv(cls.CSV_FILE)
+        df["date"] = pd.to_datetime(df["date"], format=CSV.FORMAT)
+        start_date = datetime.strptime(start_date, CSV.FORMAT)
+        end_date = datetime.strptime(end_date, CSV.FORMAT)
+
+        mask = (df["date"] >= start_date) & (df["date"] <= end_date)
+        filtered_df = df.loc[mask]
+
+        if filtered_df.empty:
+            print("No transactions found in the given date range.")
+        else:
+            print(
+                f"Transactions from {start_date.strftime(CSV.FORMAT)} to {end_date.strftime(CSV.FORMAT)}"
+            )
+            print(
+                filtered_df.to_string(
+                    index=False, formatters={"date": lambda x: x.strftime(CSV.FORMAT)}
+                )
+            )
+
+            total_income = filtered_df[filtered_df["category"] == "Income"][
+                "amount"
+            ].sum()
+            total_expense = filtered_df[filtered_df["category"] == "Expense"][
+                "amount"
+            ].sum()
+            print("\nSummary:")
+            print(f"Total Income: ${total_income:.2f}")
+            print(f"Total Expense: ${total_expense:.2f}")
+            print(f"Net Savings: ${(total_income - total_expense):.2f}")
+
+        return filtered_df
+
 
 def add():
     CSV.initialize_csv()
@@ -37,4 +73,5 @@ def add():
     description = get_description()
     CSV.add_entry(date, amount, category, description)
 
-add()
+
+CSV.get_transactions('01-01-2023', '01-08-2024')
